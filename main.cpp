3d6bc85618b29d000dc5db43c0c15ef38aef5aef
Ronin5M <maxrallison@icloud.com>
2026-04-29 23:29:44 -0700

    Building Game Loop
    Started building game loop in main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include "board.h"
 
-int main() {
-    Board* board = new Board();
+using namespace std;
 
+int main() {
+    //Game Variables
+    Board* board = new Board();
+    bool playing = true;
+    bool whiteTurn = true;
+    char sCol;
+    int sRow;
+    char eCol;
+    int eRow;
+
+
+    
+    //Opening
     board->printBoard();
+    cout << "Time for some chess! Make sure to enter pieces and targets as Coloum by Row (letter-number)!" << endl;
+    //Main Game Loop
+    while (playing) {
+        //White Turn Loop
+        while (whiteTurn) {
+            cout << "White's Move: ";
+            cin >> sCol >> sRow >> eCol >> eRow;
+            if (board->movePiece(sRow, sCol, eRow, eCol)) {
+                whiteTurn = false;
+            } else {
+                cout << "Hey that's an invalid move! Try again!";
+            }
+        }
+        //Black Turn Loop
+        while (!whiteTurn) {
+            cout << "Black's Move: ";
+            cin >> sCol >> sRow >> eCol >> eRow;
+            if (board->movePiece(sRow, sCol, eRow, eCol)) {
+                whiteTurn = true;
+            } else {
+                cout << "Hey that's an invalid move! Try again!";
+            }
+        }
+    }
 
     return 0;
 }
